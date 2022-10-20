42c1362e8145d76a55f12c9b3e8d25c869640502
litian <litian2@kylinos.cn>
2022-10-20 11:06:42 +0800

    启用高分屏适配

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "virtualkeyboardmanager.h"
 
 int main(int argc, char *argv[]) {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
     VirtualKeyboardManager virtualKeyboardManager;
     return app.exec();
