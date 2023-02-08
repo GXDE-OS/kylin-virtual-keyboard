5e6753d7ee407d51975f3107bb8cda158e098a3b
liulinsong <liulinsong@kylinos.cn>
2023-02-08 10:54:07 +0800

    完善计算虚拟键盘高度的方法
    1. 如果请求的虚拟键盘的高度小于最小合法高度，则虚拟键盘的高度为最小合法高度
    
    2. 如果请求的虚拟键盘的高度大于等于最小合法高度，则虚拟键盘的高度为请求的
    虚拟键盘高度

--- src/geometrymanager.cpp
+++ src/geometrymanager.cpp
@@ -19,10 +19,15 @@ int GeometryManager::calculateVirtualKeyboardHeight() const {
     } else {
         // 竖屏情况下，从屏幕左上角开始算起，以当前屏幕宽度为测量基准，
         // 在该范围里面的部分可以正常刷新；在该范围外面部分会出现重影。
-        // 因此，需要将虚拟键盘的高度设置为屏幕宽高差值+1，使其可以正确
-        // 刷新qml内容, 防止旋转到横屏后出现重影
+        // 因此，需要将虚拟键盘的高度设置为屏幕宽高差值+1，将其作为虚拟
+        // 键盘的最小高度,从而确保虚拟键盘可以正确刷新qml内容, 防止旋转
+        // 到横屏后出现重影
         // TODO(linyuxuan): 找出bug成因，彻底解决该问题
-        return viewPortSize.height() - viewPortSize.width() + 1;
+        const int leastHeight =
+            viewPortSize.height() - viewPortSize.width() + 1;
+        const int requiredHeight =
+            viewPortSize.height() * getVirtualKeyboardHeightRatio();
+        return std::max(leastHeight, requiredHeight);
     }
 }
 
