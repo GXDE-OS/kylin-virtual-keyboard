9124a59a0429ad58e1fbb5ed3788c261df8f4de3
liulinsong <liulinsong@kylinos.cn>
2023-02-27 15:45:09 +0800

    启用翻译功能
    1. 虚拟键盘启用翻译功能，支持简体中文和英文
    
    2. 其他尚未支持的语言默认使用英文

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
 #include "virtualkeyboardmanager.h"
 
 int main(int argc, char *argv[]) {
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
+
+    QTranslator translator;
+    if (translator.load(QLocale::system(), "translation", "_",
+                        ":/translations")) {
+        app.installTranslator(&translator);
+    }
+
     VirtualKeyboardManager virtualKeyboardManager;
     return app.exec();
 }
