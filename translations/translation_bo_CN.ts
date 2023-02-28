8ded3039fd2d30919acc853884cd7260678dfae7
liulinsong <liulinsong@kylinos.cn>
2023-02-28 13:38:58 +0800

    使用更准确的藏语翻译

--- translations/translation_bo_CN.ts
+++ translations/translation_bo_CN.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/trayentry.cpp" line="16"/>
         <source>kylin-virtual-keyboard</source>
-        <translation>རྟོག་བཟོའི་མཐེབ་གཞོང་།</translation>
+        <translation>རྟོག་བཟོས་མཐེབ་གཞུང་།</translation>
     </message>
 </context>
 </TS>
