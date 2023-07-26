d2eb757da5a37143ca1706a6c6cde70d6217f27e
hantengc <hanteng@kylinos.cn>
2023-07-26 11:16:37 +0800

    给每个文件增加license声明

--- src/virtualkeyboardentry/floatbutton.cpp
+++ src/virtualkeyboardentry/floatbutton.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2022 KylinSoft Co., Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "virtualkeyboardentry/floatbutton.h"
 
 #include <QBitmap>
@@ -100,15 +117,15 @@ void FloatButton::mouseMoveEvent(QMouseEvent *event) {
 void FloatButton::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
-    painter.setBrush(QBrush(Qt::white)); 
-    painter.setPen(Qt::transparent); 
-    
-    QRect rect = this->rect(); 
-    rect.setWidth(rect.width()); 
-    rect.setHeight(rect.height()); 
+    painter.setBrush(QBrush(Qt::white));
+    painter.setPen(Qt::transparent);
 
-    painter.drawEllipse(rect); 
-    
+    QRect rect = this->rect();
+    rect.setWidth(rect.width());
+    rect.setHeight(rect.height());
+
+    painter.drawEllipse(rect);
+
     QPushButton::paintEvent(event);
 }
 
--- src/virtualkeyboardentry/floatbutton.h
+++ src/virtualkeyboardentry/floatbutton.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2022 KylinSoft Co., Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef FLOATBUTTON_H
 #define FLOATBUTTON_H
 
@@ -5,10 +22,10 @@
 #include <memory>
 
 #include <QMouseEvent>
+#include <QPaintEvent>
 #include <QPushButton>
 #include <QSize>
 #include <QTimer>
-#include <QPaintEvent>
 
 class FloatButton : public QPushButton {
     Q_OBJECT
