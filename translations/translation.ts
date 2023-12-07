cad3675eef676c6607dcca1e3c73cc560be72c58
hantengc <hanteng@kylinos.cn>
2023-12-07 14:23:55 +0800

    新增ToolTip信息翻译

--- translations/translation.ts
+++ translations/translation.ts
@@ -2,14 +2,36 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en">
 <context>
+    <name>HideButton</name>
+    <message>
+        <location filename="../qml/HideButton.qml" line="38"/>
+        <source>close</source>
+        <translation>close</translation>
+    </message>
+</context>
+<context>
+    <name>PlacementModeButton</name>
+    <message>
+        <location filename="../qml/PlacementModeButton.qml" line="40"/>
+        <location filename="../qml/PlacementModeButton.qml" line="67"/>
+        <source>Floating Mode</source>
+        <translation>Floating Mode</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlacementModeButton.qml" line="78"/>
+        <source>Docking Mode</source>
+        <translation>Docking Mode</translation>
+    </message>
+</context>
+<context>
     <name>VirtualKeyboardEntryManager</name>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="78"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="83"/>
         <source>Disable the float button</source>
         <translation>Disable the float ball</translation>
     </message>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="85"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="93"/>
         <source>Enable the float button</source>
         <translation>Enable the float ball</translation>
     </message>
@@ -17,7 +39,7 @@
 <context>
     <name>VirtualKeyboardTrayIcon</name>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardtrayicon.cpp" line="25"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardtrayicon.cpp" line="41"/>
         <source>kylin-virtual-keyboard</source>
         <translation>Kylin Virtual Keyboard</translation>
     </message>
--- translations/translation_zh_CN.ts
+++ translations/translation_zh_CN.ts
@@ -2,14 +2,36 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>HideButton</name>
+    <message>
+        <location filename="../qml/HideButton.qml" line="38"/>
+        <source>close</source>
+        <translation>关闭</translation>
+    </message>
+</context>
+<context>
+    <name>PlacementModeButton</name>
+    <message>
+        <location filename="../qml/PlacementModeButton.qml" line="40"/>
+        <location filename="../qml/PlacementModeButton.qml" line="67"/>
+        <source>Floating Mode</source>
+        <translation>悬浮状态</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlacementModeButton.qml" line="78"/>
+        <source>Docking Mode</source>
+        <translation>贴边状态</translation>
+    </message>
+</context>
+<context>
     <name>VirtualKeyboardEntryManager</name>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="78"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="83"/>
         <source>Disable the float button</source>
         <translation>禁用键盘悬浮球</translation>
     </message>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="85"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="93"/>
         <source>Enable the float button</source>
         <translation>启用键盘悬浮球</translation>
     </message>
@@ -17,7 +39,7 @@
 <context>
     <name>VirtualKeyboardTrayIcon</name>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardtrayicon.cpp" line="25"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardtrayicon.cpp" line="41"/>
         <source>kylin-virtual-keyboard</source>
         <translation>麒麟虚拟键盘</translation>
     </message>
