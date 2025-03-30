e7948a05f02770858c8744e406f9c97ad7959ff8
hantengc <hanteng@kylinos.cn>
2025-03-30 11:45:23 +0800

    add(ipc): add ukui framework dbus service proxy

---
+++ src/ipc/ukuiframeworkserviceproxy.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) KylinSoft Co., Ltd. 2025.All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "ukuiframeworkserviceproxy.h"
+
+const QString g_serviceName = "org.ukui.Framework";
+const QString g_servicePath = "/org/ukui/Framework/Devices/Keyboard";
+const char *g_serviceInterface = "org.ukui.Framework.Devices.Keyboard";
+
+const QString g_kbdCountMethodName = "GetKbdCount";
+const QString g_kbdStatusSignalName = "KbdCountChanged";
+
+UKUIFrameworkServiceProxy::UKUIFrameworkServiceProxy(QObject *parent)
+    : QDBusAbstractInterface(g_serviceName, g_servicePath, g_serviceInterface,
+                             QDBusConnection::sessionBus(), parent) {
+    QDBusConnection::sessionBus().connect(
+        g_serviceName, g_servicePath, g_serviceInterface, g_kbdStatusSignalName,
+        this, SIGNAL(kbdStatusChanged()));
+}
+
+QDBusPendingReply<int> UKUIFrameworkServiceProxy::GetKbdCount() {
+    QList<QVariant> argumentList;
+
+    qDebug() << "DbusServiceProxy"
+             << "func: " << __FUNCTION__ << " line: ";
+    return asyncCallWithArgumentList(g_kbdCountMethodName, argumentList);
+}
---
+++ src/ipc/ukuiframeworkserviceproxy.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) KylinSoft Co., Ltd. 2025.All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _UKUIFREAMEWORKSERVICEPROXY_H_
+#define _UKUIFREAMEWORKSERVICEPROXY_H_
+
+#include <QDBusAbstractInterface>
+#include <QDBusInterface>
+#include <QDBusPendingReply>
+#include <QDebug>
+#include <QObject>
+
+class UKUIFrameworkServiceProxy : public QDBusAbstractInterface {
+    Q_OBJECT
+public:
+    explicit UKUIFrameworkServiceProxy(QObject *parent = nullptr);
+    ~UKUIFrameworkServiceProxy() override = default;
+
+Q_SIGNALS:
+    void kbdStatusChanged();
+
+public Q_SLOTS:
+    QDBusPendingReply<int> GetKbdCount();
+};
+
+#endif
