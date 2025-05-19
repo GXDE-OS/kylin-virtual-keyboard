515c829de1ff65d25d3b2c07a0ecf2f46952ccbb
niushijia <niushijia@kylinos.cn>
2025-05-19 15:33:17 +0800

    add:mn translation

--- translations/translation_mn.ts
+++ translations/translation_mn.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="mn">
 <context>
+    <name>FloatButton</name>
+    <message>
+        <location filename="../src/virtualkeyboardentry/floatbutton.cpp" line="148"/>
+        <source>Click to show virtual keyboard</source>
+        <translation>ᠵᠢᠱᠢᠮᠡᠭ ᠳᠠᠷᠤᠭᠤᠯ ᠢ᠋ ᠢᠯᠡᠷᠡᠬᠦᠯᠥᠬᠦ</translation>
+    </message>
+</context>
+<context>
     <name>HideButton</name>
     <message>
         <location filename="../qml/HideButton.qml" line="38"/>
@@ -15,33 +23,33 @@
         <location filename="../qml/PlacementModeButton.qml" line="40"/>
         <location filename="../qml/PlacementModeButton.qml" line="67"/>
         <source>Floating Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>ᠬᠥᠪᠥᠮᠡᠯ ᠪᠠᠢᠳᠠᠯ</translation>
     </message>
     <message>
         <location filename="../qml/PlacementModeButton.qml" line="78"/>
         <source>Docking Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>ᠨᠠᠭᠠᠮᠠᠯ ᠪᠠᠢᠳᠠᠯ</translation>
     </message>
 </context>
 <context>
     <name>VirtualKeyboardEntryManager</name>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="83"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="98"/>
         <source>Disable the float button</source>
-        <translation type="unfinished"></translation>
+        <translation>ᠳᠠᠷᠤᠭᠤᠯ ᠤ᠋ᠨ ᠬᠥᠪᠥᠮᠡᠯ ᠪᠠᠢᠳᠠᠯ ᠢ᠋ ᠴᠠᠭᠠᠵᠠᠯᠠᠬᠤ</translation>
     </message>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="93"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="108"/>
         <source>Enable the float button</source>
-        <translation type="unfinished"></translation>
+        <translation>ᠳᠠᠷᠤᠭᠤᠯ ᠤ᠋ᠨ ᠬᠥᠪᠥᠮᠡᠯ ᠪᠠᠢᠳᠠᠯ ᠢ᠋ ᠡᠬᠢᠯᠡᠬᠦᠯᠬᠦ</translation>
     </message>
 </context>
 <context>
     <name>VirtualKeyboardTrayIcon</name>
     <message>
-        <location filename="../src/virtualkeyboardentry/virtualkeyboardtrayicon.cpp" line="41"/>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardtrayicon.cpp" line="73"/>
         <source>kylin-virtual-keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>ᠴᠢ ᠯᠢᠨ ᠵᠢᠱᠢᠮᠡᠭ ᠳᠠᠷᠤᠭᠤᠯ</translation>
     </message>
 </context>
 </TS>
