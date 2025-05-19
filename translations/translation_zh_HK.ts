d27b8d913b1e36f793784150940b79344e95495e
niushijia <niushijia@kylinos.cn>
2025-05-19 15:32:59 +0800

    add:zh_HK translation

---
+++ translations/translation_zh_HK.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_HK">
+<context>
+    <name>FloatButton</name>
+    <message>
+        <location filename="../src/virtualkeyboardentry/floatbutton.cpp" line="148"/>
+        <source>Click to show virtual keyboard</source>
+        <translation>顯示虛擬鍵盤</translation>
+    </message>
+</context>
+<context>
+    <name>HideButton</name>
+    <message>
+        <location filename="../qml/HideButton.qml" line="38"/>
+        <source>close</source>
+        <translation>關閉</translation>
+    </message>
+</context>
+<context>
+    <name>PlacementModeButton</name>
+    <message>
+        <location filename="../qml/PlacementModeButton.qml" line="40"/>
+        <location filename="../qml/PlacementModeButton.qml" line="67"/>
+        <source>Floating Mode</source>
+        <translation>懸浮狀態</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlacementModeButton.qml" line="78"/>
+        <source>Docking Mode</source>
+        <translation>貼邊狀態</translation>
+    </message>
+</context>
+<context>
+    <name>VirtualKeyboardEntryManager</name>
+    <message>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="98"/>
+        <source>Disable the float button</source>
+        <translation>禁用鍵盤懸浮球</translation>
+    </message>
+    <message>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardentrymanager.cpp" line="108"/>
+        <source>Enable the float button</source>
+        <translation>啟用鍵盤懸浮球</translation>
+    </message>
+</context>
+<context>
+    <name>VirtualKeyboardTrayIcon</name>
+    <message>
+        <location filename="../src/virtualkeyboardentry/virtualkeyboardtrayicon.cpp" line="73"/>
+        <source>kylin-virtual-keyboard</source>
+        <translation>麒麟虛擬鍵盤</translation>
+    </message>
+</context>
+</TS>
