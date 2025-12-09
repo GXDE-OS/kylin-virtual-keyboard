59001515151a059e461c5c31335d1e8f7dde8e3a
niushijia <niushijia@kylinos.cn>
2025-12-09 14:03:00 +0800

    fix: 同步内网的代码review修改

--- src/utils.h
+++ src/utils.h
@@ -1,35 +1,47 @@
+/*
+ * Copyright (c) KylinSoft Co., Ltd. 2025.All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <algorithm>
+#include <cctype>
+#include <cstring>
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <cctype>
-#include <algorithm>
 #include <vector>
 
-enum class DesktopType {
-    X11,
-    WAYLAND,
-    UNKNOWN
-};
+enum class DesktopType { X11, WAYLAND, UNKNOWN };
 
-enum class DesktopEnvironment {
-    GNOME,
-    KDE,
-    UKUI,
-    UNKNOWN
-};
+enum class DesktopEnvironment { GNOME, KDE, UKUI, UNKNOWN };
 
-static std::string toLower(const std::string& str) {
+static std::string toLower(const char *str) {
+    if (str == nullptr) {
+        return std::string();
+    }
     std::string result = str;
     std::transform(result.begin(), result.end(), result.begin(),
-                  [](unsigned char c) { return std::tolower(c); });
+                   [](unsigned char c) { return std::tolower(c); });
     return result;
 }
 
-static std::vector<std::string> splitString(const std::string& str, char delimiter) {
+static std::vector<std::string> splitString(const std::string &str,
+                                            char delimiter) {
     std::vector<std::string> tokens;
     size_t start = 0;
     size_t end = str.find(delimiter);
-    
+
     while (end != std::string::npos) {
         if (end != start) {
             tokens.push_back(str.substr(start, end - start));
@@ -37,25 +49,20 @@ static std::vector<std::string> splitString(const std::string& str, char delimit
         start = end + 1;
         end = str.find(delimiter, start);
     }
-    
+
     if (start < str.length()) {
         tokens.push_back(str.substr(start));
     }
-    
+
     return tokens;
 }
 
 static inline DesktopEnvironment getDesktopEnvironment() {
-    //在ubuntu 上该环境变量会返回gnome:GNOME
-    const char* xdgDesktop = std::getenv("XDG_CURRENT_DESKTOP");
-    if (xdgDesktop == nullptr) {
-        return DesktopEnvironment::UNKNOWN;
-    }
-
-    std::string lowerDesktop = toLower(xdgDesktop);
+    // 在ubuntu 上该环境变量会返回gnome:GNOME
+    std::string lowerDesktop = toLower(std::getenv("XDG_CURRENT_DESKTOP"));
     auto desktops = splitString(lowerDesktop, ':');
-    
-    for (const auto& name : desktops) {
+
+    for (const auto &name : desktops) {
         if (name == "kde") {
             return DesktopEnvironment::KDE;
         } else if (name == "gnome") {
@@ -64,22 +71,17 @@ static inline DesktopEnvironment getDesktopEnvironment() {
             return DesktopEnvironment::UKUI;
         }
     }
-    
+
     return DesktopEnvironment::UNKNOWN;
 }
 
 static inline DesktopType getDesktopType() {
-    const char* sessionType = std::getenv("XDG_SESSION_TYPE");
-    if (sessionType == nullptr) {
-        return DesktopType::UNKNOWN;
-    }
-    
-    std::string type = toLower(sessionType);
+    std::string type = toLower(std::getenv("XDG_SESSION_TYPE"));
     if (type == "x11") {
         return DesktopType::X11;
     } else if (type == "wayland") {
         return DesktopType::WAYLAND;
     }
-    
+
     return DesktopType::UNKNOWN;
 }
