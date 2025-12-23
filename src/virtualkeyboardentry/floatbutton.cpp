84ea4d9bf03280c8c676e0614cb86bc25e301e5e
hantengc <hanteng@kylinos.cn>
2025-12-23 17:52:11 +0800

    fix: 修复笔误

--- src/virtualkeyboardentry/floatbutton.cpp
+++ src/virtualkeyboardentry/floatbutton.cpp
@@ -15,7 +15,7 @@
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "virtualkeyboardentry/floatbutton.h"
+#include "floatbutton.h"
 
 #include <QBitmap>
 #include <QPainter>
